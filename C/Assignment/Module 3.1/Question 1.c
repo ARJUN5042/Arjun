27b7395fb4ad4e69623d064060ea363b555ae2f5
ARJUN5042 <hirparaarjun5042@gmail.com>
2022-12-09 09:08:07 +0530

    re

--- C/Assignment/Module 3.1/Question 1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main()
-{
-	printf("\n\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 Bio \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\n\n");
-	printf("Name :- Arjun Kantibhai Hirpara\n");
-	printf("Birth Date :- 28/10/1996\n");
-	printf("Age :- 26\n");
-	printf("Address :- Radhe Krishna Appartment, C.G.Road, Ahmedabad");
-}
