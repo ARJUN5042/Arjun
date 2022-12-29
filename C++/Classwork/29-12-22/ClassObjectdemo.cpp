babe90a4db2e3751ca47959a66bcf636f30d9e4e
ARJUN5042 <hirparaarjun5042@gmail.com>
2022-12-29 20:41:15 +0530

    29-12-22

---
+++ C++/Classwork/29-12-22/ClassObjectdemo.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Student   // Class Creation
+{
+	public:		
+		int a=10;
+};
+
+int main()
+{
+	Student std;  // Object Creation
+	cout<<std.a;
+	return 0;
+}
