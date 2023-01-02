0395775ce91dc21c27f50f7853ab531ad7362e40
ARJUN5042 <hirparaarjun5042@gmail.com>
2023-01-02 16:51:07 +0530

    Assignment

--- C++/Assignment/Module 4/Module 4.2/Q1.Simple Calculator using Class.cpp
+++ C++/Assignment/Module 4/Module 4.2/Q1.Simple Calculator using Class.cpp
@@ -1,75 +1,30 @@
 #include<iostream>
 using namespace std;
-//	creating class
 class calculator
 {
 	public:
-	int a,b;
-	
-//	creating function for input value from user and display
-	void input()
-	{
-		cout<<"Enter A : ";
-		cin>>a;
-		cout <<"Enter B : ";
-		cin>>b;
-		cout<<"\nA : "<<a<<"\tB : "<<b;
-	}
-//	creating addition function
-	void add()
-	{
-		cout<<endl<<"Additon : "<<a<<" + "<<b<<" = "<<(a+b);
-	}
-//	creating subtraction function
-	void sub()
-	{
-		cout<<endl<<"Subtraction : "<<a<<" - "<<b<<" = "<<(a-b);
-	}
-//	creating multiplication function
-	void mul()
-	{
-		cout<<endl<<"Multiplication : "<<a<<" * "<<b<<" = "<<(a*b);
-	}
-//	creating division function
-	void div()
-	{
-		cout<<endl<<"Division : "<<a<<" / "<<b<<" = "<<((float)a/b);
-	}
+		double x,y;
+		void display(double x, double y)
+		{
+			cout<<"\n Addition : "<<x<<" + "<<y<<" = "<<(x+y);
+			cout<<"\n Subtraction : "<<x<<" - "<<y<<" = "<<(x-y);
+			cout<<"\n Multiplication : "<<x<<" * "<<y<<" = "<<(x*y);
+			cout<<"\n Division : "<<x<<" / "<<y<<" = "<<(x/y);
+		}
+		
 };
-
-
 int main()
 {
-	cout<<"\n******Simple Calculator******"<<endl<<endl;
 	calculator calc;
-	calc.input();
-	int n;
-	cout<<endl<<endl<<"\tChoice Menu"<<endl;
-	cout<<endl<<"Press 1 for Addition";
-	cout<<endl<<"Press 2 for Suntraction";
-	cout<<endl<<"Press 3 for Multiplication";
-	cout<<endl<<"Press 4 for Division";
-	
-	cout<<endl<<endl<<"Enter your choice ? :-->  ";
-	cin>>n;
-	
-	switch(n)
-	{
-		case 1:
-			calc.add();
-			break;
-		case 2:
-			calc.sub();
-			break;
-		case 3:
-			calc.mul();
-			break;
-		case 4:
-			calc.div();
-			break;
-		default:
-			cout<<endl<<endl<<"INVALID INPUT !!!!!!!";
-			break;
-	}
+	double a,b;
+	cout<<"\n\n\tSimple Calculator";
+	cout<<"\n------------------------------------";
+	cout<<"\n Enter A : ";
+	cin>>a;
+	cout<<"\n Enter B : ";
+	cin>>b;
+	cout<<endl<<"\n A : "<<a<<"\t\tB : "<<b;
+	cout<<"\n------------------------------------";
+	calc.display(a,b);
 	return 0;
 }
---
+++ C++/Assignment/Module 4/Module 4.2/Q3.Multiplication and cubic value with inline function.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+//	creating class
+class line
+{
+	public:
+		
+//		creating inline function for multiplication value
+		inline float mul(float x, float y)
+		{
+			return(x*y);
+		}
+		
+//		creating inline function for Cubic value
+		inline float cube(float x)
+		{
+			return(x*x*x);
+		}
+};
+
+int main()
+{
+//	creating object
+	line m_c;
+	
+//	creating variables
+	float a,b;
+	
+//	input value
+	cout<<"\n Enter A : ";
+	cin>>a;
+	cout<<"\n Enter B : ";
+	cin>>b;
+	
+//	display value
+	cout<<"\n A : "<<a<<"\t B : "<<b;
+	
+	cout<<"\n------------------------------";
+//	calling member function of multiplication value
+	cout<<"\n\n Multiplication Value";
+	cout<<"\n "<<a<<"*"<<b<<" : "<<m_c.mul(a,b)<<endl;
+	
+	cout<<"\n------------------------------";
+//	calling member function of cubic value
+	cout<<"\n\n Cube Value";
+	cout<<"\n "<<a<<"^3 : "<<m_c.cube(a);
+	cout<<"\n "<<b<<"^3 : "<<m_c.cube(b);
+	return 0;
+}
