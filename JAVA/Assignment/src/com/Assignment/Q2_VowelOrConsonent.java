da398cc74ab10858f6362e1fc6ed5f485680f9e1
ARJUN5042 <hirparaarjun5042@gmail.com>
2023-04-28 11:26:49 +0530

    Assignments

--- JAVA/Assignment/src/com/Assignment/Q2_VowelOrConsonent.java
+++ JAVA/Assignment/src/com/Assignment/Q2_VowelOrConsonent.java
@@ -3,19 +3,5 @@ import java.util.Scanner;
 
 public class Q2_VowelOrConsonent 
 {
-	public static void main(String[] args) 
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Alphaber: ");
-		String ch=sc.next();
-		
-		if(ch=='a')
-		{
-			System.out.println("Vowel");
-		}
-		else
-		{
-			System.out.println("Consonant");
-		}
-	}
+	
 }
