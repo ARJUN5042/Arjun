337877896871b042e96ac0161ad4b90bf6e250d4
ARJUN5042 <hirparaarjun5042@gmail.com>
2023-05-15 12:28:21 +0530

    Assignment

--- JAVA/Assignment/src/com/Assignment/Q2_VowelOrConsonent.java
+++ JAVA/Assignment/src/com/Assignment/Q2_VowelOrConsonent.java
@@ -10,26 +10,13 @@ public class Q2_VowelOrConsonent
 		System.out.println("Enter a Single Character : ");
 		String input = sc.next();
 		
-		boolean uppercase = input.charAt(0)>=65 && input.charAt(0)<=90;
-		boolean lowercase = input.charAt(0)>=97 && input.charAt(0)<=122;
-		boolean vowels = input.equals("a")||input.equals("A")||input.equals("e")||input.equals("E")||input.equals("i")||input.equals("I")||input.equals("o")||input.equals("O")||input.equals("u")||input.equals("U");
-		
-		if(input.length()>1)
+//		boolean uppercase = input.charAt(0)>=65 && input.charAt(0)<=90;
+//		boolean lowercase = input.charAt(0)>=97 && input.charAt(0)<=122;
+//		boolean vowels = input.equals("a")||input.equals("A")||input.equals("e")||input.equals("E")||input.equals("i")||input.equals("I")||input.equals("o")||input.equals("O")||input.equals("u")||input.equals("U");
+//		
+		if(input.length()!=1)
 		{
-			System.out.println("Error: Input more than 1 character.");
+			System.out.println("You Entered morethan one character...");
 		}
-		else if(!(uppercase || lowercase))
-		{
-			System.out.println("Error: Input character is Not an Alphabet..");
-		}
-		else if(vowels)
-		{
-			System.out.println("Input letter is Vowel.");
-		}
-		else
-		{
-			System.out.println("Input letter is Consonant.");
-		}
-		sc.close();
 	}
 }
