3afb2a7a5011c8c118db153e72669e8174d00ca2
ARJUN5042 <hirparaarjun5042@gmail.com>
2023-08-04 10:33:28 +0530

    Work

--- JAVA/Module 4 Assignment/src/main/java/com/assignment/Q2_ServletContextDemo.java
+++ JAVA/Module 4 Assignment/src/main/java/com/assignment/Q2_ServletContextDemo.java
@@ -4,35 +4,34 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//@WebServlet("/Q1_ServletConfigDemo")
-public class Q1_ServletConfigDemo extends HttpServlet {
-	ServletConfig config=null;
+@WebServlet("/Q2_ServletContextDemo")
+public class Q2_ServletContextDemo extends HttpServlet {
+	ServletContext context=null;
 	private static final long serialVersionUID = 1L;
-    public Q1_ServletConfigDemo() {
+    public Q2_ServletContextDemo() {
         super();
         // TODO Auto-generated constructor stub
     }
-    @Override
     public void init(ServletConfig config) throws ServletException 
     {
-    	this.config=config;
+    	context = config.getServletContext();
     }
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
 		response.setContentType("text/html");
 		PrintWriter out=response.getWriter();
-		String username=config.getInitParameter("username");
-		String password=config.getInitParameter("password");
-		out.println("Username is : "+username);
-		out.println("<br>Password is : "+password);
+		String dburl=context.getInitParameter("dburl");
+		String dbusername=context.getInitParameter("dbusername");
+		out.println("DBURL is : "+dburl);
+		out.println("<br>DBUsername is : "+dbusername);
 	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		
 	}
---
+++ JAVA/Module 4 Assignment/src/main/webapp/Q4_FetchDataFromAnOtherJSPFile.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Student Information</title>
+</head>
+<body>
+<h1>Student Information</h1>
+<p>First Name: ${firstname }</p>
+<p>Last Name: ${lastname }</p>
+<p>Email: ${email }</p>
+<p>Mobile: ${mobile }</p>
+<p>Gender: ${gender }</p>
+<p>Password: ${password }</p>
+</body>
+</html>
